e95310d6d875cf82b170ba429e88e1922c676aa3
biswajitpuhan90 <biswajitpuhan911@gmail.com>
2025-12-19 20:23:01 +0530

    Update Biodata.java

--- Biodata.java
+++ Biodata.java
@@ -1,12 +1,7 @@
-class Biodata{
-    public static void main(String[] args){
-         System.out.println("Wel Come To BiJesh");
-         System.out.println("Namaste Sirmy self Bijesh Rout");
-         System.out.println("My Mother Name-Pramila Rout");
-         System.out.println("My Father Name-Debendra Rout");
-         System.out.println(" coming From Cuttack");
-         System.out.println("My Qulification B-Tesh Civil");
-         System.out.println("And My Ex-5yrs at Construction");
-         System.out.println("Currently job in BBRS");
-   }
-}
+Pfroduct sale life
+    Laptop------34000
+    Phonex-------45000
+    Refrigetor-----35000
+    LED TV------21000
+    phonese------23000
+    
